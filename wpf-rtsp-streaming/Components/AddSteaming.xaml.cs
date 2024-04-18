03063c9f6c9ad91159c2c350abaeafe44b8381bd
Min.Hsieh <Min.Hsieh@advantech.com.tw>
2024-04-18 20:19:16 +0800

    Add Add Streaming component

---
+++ wpf-rtsp-streaming/Components/AddSteaming.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Win32;
+using Min_Helpers;
+using Min_Helpers.PrintHelper;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace wpf_rtsp_streaming.Components
+{
+    /// <summary>
+    /// AddSteaming.xaml 的互動邏輯
+    /// </summary>
+    public partial class AddSteaming : UserControl
+    {
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilePath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilePathProperty =
+            DependencyProperty.Register("FilePath", typeof(string), typeof(AddSteaming), new PropertyMetadata(null));
+
+        public string RTSPPath
+        {
+            get { return (string)GetValue(RTSPPathProperty); }
+            set { SetValue(RTSPPathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RTSPPath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RTSPPathProperty =
+            DependencyProperty.Register("RTSPPath", typeof(string), typeof(AddSteaming), new PropertyMetadata(null));
+
+        public class StreamingEventArgs : EventArgs
+        {
+            public string FilePath { get; set; }
+            public string RTSPPath { get; set; }
+
+            public StreamingEventArgs(string filePath, string rTSPPath)
+            {
+                FilePath = filePath;
+                RTSPPath = rTSPPath;
+            }
+        }
+
+        public event EventHandler<StreamingEventArgs> Save;
+        public event EventHandler Cancel;
+
+        public AddSteaming()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Select Video File";
+
+                bool? dialogResult = dialog.ShowDialog();
+                if (dialogResult != true)
+                {
+                    return;
+                }
+
+                string filePath = dialog.FileName;
+
+                this.FilePath = filePath;
+            }
+            catch (Exception ex)
+            {
+                ex = ExceptionHelper.GetReal(ex);
+                string message = ex.Message;
+
+                App.PrintService.Log($"{this.GetType().Name}, {message}", Print.EMode.error);
+
+                MessageBox.Show(message, App.AppName, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.Save != null)
+                {
+                    this.Save(this, new StreamingEventArgs(this.FilePath, this.RTSPPath));
+
+                    this.ButtonCancel_Click(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex = ExceptionHelper.GetReal(ex);
+                string message = ex.Message;
+
+                App.PrintService.Log($"{this.GetType().Name}, {message}", Print.EMode.error);
+
+                MessageBox.Show(message, App.AppName, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.Cancel != null)
+                {
+                    this.Cancel(this, EventArgs.Empty);
+
+                    this.FilePath = null;
+                    this.RTSPPath = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex = ExceptionHelper.GetReal(ex);
+                string message = ex.Message;
+
+                App.PrintService.Log($"{this.GetType().Name}, {message}", Print.EMode.error);
+
+                MessageBox.Show(message, App.AppName, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}

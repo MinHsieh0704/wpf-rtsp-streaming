8b9e27fa331c0d81912218dc15a9c0c8575eea04
Min.Hsieh <Min.Hsieh@advantech.com.tw>
2024-05-02 18:12:19 +0800

    Update Streaming helper add youtube support

--- wpf-rtsp-streaming/Helpers/Streaming.cs
+++ wpf-rtsp-streaming/Helpers/Streaming.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Management;
 using System.Reactive.Subjects;
 using System.Text.RegularExpressions;
 
@@ -57,6 +58,28 @@ namespace wpf_rtsp_streaming.Helpers
         {
             try
             {
+                if (filePath.IndexOf("https://www.youtube.com") > -1)
+                {
+                    this.ConnectWithYoutube();
+                }
+                else
+                {
+                    this.ConnectWithFile();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Connect With File
+        /// </summary>
+        private void ConnectWithFile()
+        {
+            try
+            {
                 string file = $"{AppDomain.CurrentDomain.BaseDirectory}mediamtx\\ffmpeg.exe";
                 if (!File.Exists(file))
                 {
@@ -109,6 +132,100 @@ namespace wpf_rtsp_streaming.Helpers
         }
 
         /// <summary>
+        /// Connect With Youtube
+        /// </summary>
+        private void ConnectWithYoutube()
+        {
+            try
+            {
+                string file = $"{AppDomain.CurrentDomain.BaseDirectory}mediamtx\\yt-dlp.exe";
+                if (!File.Exists(file))
+                {
+                    throw new Exception($"Can not found {file}");
+                }
+
+                file = $"{AppDomain.CurrentDomain.BaseDirectory}mediamtx\\ffmpeg.exe";
+                if (!File.Exists(file))
+                {
+                    throw new Exception($"Can not found {file}");
+                }
+
+                this.process = new Process();
+
+                this.process.StartInfo.FileName = "cmd.exe";
+                this.process.StartInfo.UseShellExecute = false;
+                this.process.StartInfo.RedirectStandardOutput = true;
+                this.process.StartInfo.RedirectStandardError = true;
+                this.process.StartInfo.RedirectStandardInput = true;
+                this.process.StartInfo.CreateNoWindow = true;
+                this.process.StartInfo.WorkingDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}mediamtx";
+
+                this.process.EnableRaisingEvents = true;
+
+                this.process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        this.onMessage.OnNext(e.Data);
+                    }
+                };
+                this.process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        string message = e.Data;
+                        if (Regex.IsMatch(message.ToLower(), "fail|error", RegexOptions.IgnoreCase))
+                        {
+                            this.onError.OnNext(new Exception(message));
+                            return;
+                        }
+
+                        this.onMessage.OnNext(e.Data);
+                    }
+                };
+
+                this.process.Start();
+                this.processId = this.process.Id;
+
+                this.process.BeginOutputReadLine();
+                this.process.BeginErrorReadLine();
+
+                this.process.StandardInput.WriteLine($"yt-dlp.exe -o - \"{this.filePath}\" | ffmpeg.exe -re -stream_loop -1 -i pipe: -c copy -rtsp_transport tcp -f rtsp rtsp://127.0.0.1:8554/{this.rtspPath}");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Kill Child Process
+        /// </summary>
+        /// <param name="processId"></param>
+        private void KillChildProcess(int processId)
+        {
+            try
+            {
+                ManagementObjectSearcher managementSearcher = new ManagementObjectSearcher(String.Format("Select * From Win32_Process Where ParentProcessID={0}", processId));
+                foreach (ManagementObject managementObject in managementSearcher.Get())
+                {
+                    Process process = Process.GetProcessById(Convert.ToInt32(managementObject["ProcessID"]));
+
+                    this.KillChildProcess(process.Id);
+
+                    if (!process.HasExited) process.Kill();
+                    process.Close();
+                    process.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
         /// Disconnect
         /// </summary>
         public void Disconnect()
@@ -119,6 +236,8 @@ namespace wpf_rtsp_streaming.Helpers
                 {
                     this.process.CancelOutputRead();
                     this.process.CancelErrorRead();
+
+                    this.KillChildProcess(this.process.Id);
 
                     if (!this.process.HasExited) this.process.Kill();
                     this.process.Close();
